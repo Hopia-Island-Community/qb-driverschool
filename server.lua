bc02fab01eefdce68cd5b94a3221e53edd3d87d1
Léo Brossard <leo_brossard@hotmail.fr>
2022-07-07 21:43:04 +0200

    adding version check

--- server.lua
+++ server.lua
@@ -27,3 +27,49 @@ RegisterNetEvent('driverschool:server:addLicense', function(type)
 
     Player.Functions.SetMetaData('licences', licences)
 end)
+
+local label =[[
+  ||
+  ||    ____    ____        ____  ____  _____    ____________  _____ ________  ______  ____  __
+  ||   / __ \  / __ )      / __ \/ __ \/  _/ |  / / ____/ __ \/ ___// ____/ / / / __ \/ __ \/ /
+  ||  / / / / / __  |_____/ / / / /_/ // / | | / / __/ / /_/ /\__ \/ /   / /_/ / / / / / / / /
+  || / /_/ / / /_/ /_____/ /_/ / _, _// /  | |/ / /___/ _, _/___/ / /___/ __  / /_/ / /_/ / /___
+  || \___\_\/_____/     /_____/_/ |_/___/  |___/_____/_/ |_|/____/\____/_/ /_/\____/\____/_____/
+  ||
+  ||                                    Created by Devix
+  ||
+]]
+
+-- Grabs the latest version number from the web GitHub
+PerformHttpRequest( "https://skrilax91.github.io/qb-driverschool", function( err, body, headers )
+	-- Wait to reduce spam
+	Citizen.Wait( 2000 )
+
+	-- Get the current resource version
+	local curVer = GetResourceMetadata( GetCurrentResourceName(), "version" )
+	local latest = body
+
+	label = label .. "  ||    Current version: " .. curVer
+
+	if ( latest ~= nil ) then
+		-- Print latest version
+
+		label = label .. "\n  ||    Latest recommended version: " .. latest
+
+		-- If the versions are different, print it out
+		if ( latest ~= curVer ) then
+			label = label .. "\n  ||    ^1Your qb-driverschool version is outdated, please update as soon as possible.^0"
+		else
+			label = label .. "\n  ||    ^2Qb-driverschool is up to date!^0\n"
+		end
+	else
+		label = label .. "\n  ||    ^1There was an error getting the latest version information.^0\n"
+	end
+
+	print(label)
+
+	-- Warn the console if the resource has been renamed, as this will cause issues with the resource's functionality.
+	if ( GetCurrentResourceName() ~= "qb-driverschool" ) then
+		print( "^1ERROR: Resource name is not qb-driverschool, expect there to be issues with the resource. To ensure there are no issues, please leave the resource name as qb-driverschool^0\n\n" )
+	end
+end )
